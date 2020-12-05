b9c872d3025e27220b69a84dadac1c39595269cf
JaviusssBesi <javierbesada88@gmail.com>
2020-12-05 17:35:02 +0100

    5/12/2020

---
+++ shooter(audio).py
@@ -0,0 +1,103 @@
+import pygame, random
+pygame.init()
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("meteor.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("player.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        mouse_pos = pygame.mouse.get_pos()
+        self.rect.x = mouse_pos[0]
+        self.rect.y = 510
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("laser.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.y -= 5
+
+#Definir colores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+size = (900, 600)
+
+#Crear ventana
+screen = pygame.display.set_mode(size)
+
+clock = pygame.time.Clock()
+
+done = False
+
+score = 0
+
+all_sprite_list = pygame.sprite.Group()
+meteor_list = pygame.sprite.Group()
+laser_list = pygame.sprite.Group()
+
+for i in range(50):
+    meteor = Meteor()
+    meteor.rect.x = random.randrange(880)
+    meteor.rect.y = random.randrange(450)
+
+    meteor_list.add(meteor)
+    all_sprite_list.add(meteor)
+
+sound = pygame.mixer.Sound("laser5.ogg")
+
+player = Player()
+all_sprite_list.add(player)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            laser = Laser()
+            laser.rect.x = player.rect.x + 45
+            laser.rect.y = player.rect.y - 20
+
+            all_sprite_list.add(laser)
+            laser_list.add(laser)
+            sound.play()
+
+    all_sprite_list.update()
+
+    for laser in laser_list:
+        meteor_hit_list = pygame.sprite.spritecollide(laser, meteor_list, True)
+
+        for meteor in meteor_hit_list:
+            all_sprite_list.remove(laser)
+            laser_list.remove(laser)
+            score += 1
+            print(score)
+        if laser.rect.y < -20:
+            all_sprite_list.remove(laser)
+            laser_list.remove(laser)
+
+    screen.fill(WHITE)
+    all_sprite_list.draw(screen)
+
+
+    #Actualizar pantalla
+    pygame.display.flip()
+
+    clock.tick(60)
